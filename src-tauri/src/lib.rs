4f12cc35bb1ba7adf1ccf63208cfd1a8a3d40851
Coxxon <allan.lg@outlook.fr>
2026-03-28 00:23:59 +0100

    feat: ajout commande rust get_network_interfaces

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,40 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use get_if_addrs::get_if_addrs;
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct NetworkInterface {
+    name: String,
+    ip: String,
+    mask: String,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_network_interfaces() -> Vec<NetworkInterface> {
+    let mut interfaces = Vec::new();
+
+    if let Ok(if_addrs) = get_if_addrs() {
+        for iface in if_addrs {
+            // We only care about IPv4 and non-loopback interfaces for AES67/SAP
+            if !iface.is_loopback() {
+                if let get_if_addrs::IfAddr::V4(v4_addr) = iface.addr {
+                    interfaces.push(NetworkInterface {
+                        name: iface.name,
+                        ip: v4_addr.ip.to_string(),
+                        mask: v4_addr.netmask.to_string(),
+                    });
+                }
+            }
+        }
+    }
+
+    interfaces
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_network_interfaces])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
